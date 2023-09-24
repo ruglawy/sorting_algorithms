2987cab40c9906e1a26423cdc25dcafdf595b05d
Ruglawy <git@github.com>
2023-09-24 15:55:52 -0700

    insortsort"

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - ...
+ *
+ * @list: ...
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *ptr, *temp;
+
+	if (!list)
+	{
+		return;
+	}
+
+	ptr = *list;
+	while (ptr)
+	{
+		while (ptr->next && (ptr->n > ptr->next->n))
+		{
+			temp = ptr->next;
+			ptr->next = temp->next;
+			temp->prev = ptr->prev;
+
+			if (ptr->prev)
+			{
+				ptr->prev->next = temp;
+			}
+			if (temp->next)
+			{
+				temp->next->prev = ptr;
+			}
+
+			ptr->prev = temp;
+			temp->next = ptr;
+
+			if (temp->prev)
+			{
+				ptr = temp->prev;
+			}
+			else
+			{
+				*list = temp;
+			}
+			print_list(*list);
+		}
+		ptr = ptr->next;
+	}
+}
